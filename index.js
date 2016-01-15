305666638c9c9fa1dbc10a016f868351ac157562
Amr <eldib.amr@gmail.com>
2016-01-14 23:57:44 -0800

    Set up faking data based on multi-file schema

---
+++ index.js
@@ -0,0 +1,27 @@
+﻿var jsf = require('json-schema-faker')
+var fs = require('fs')
+var readMultipleFiles = require('read-multiple-files')
+
+fs.readFile('schema/userContent.json', function (err, schemaFileContent) {
+
+  readMultipleFiles(['schema/folder.json', 'schema/extent.json', 'schema/itemSummary.json', 'schema/coordinate.json'], 'utf8',
+  (err, contents) => {
+    if (err) {
+      throw err;
+    }
+
+    var schema = JSON.parse(schemaFileContent)
+
+    var refs = []
+    contents.forEach(function (refFileContent) {
+      refs.push(JSON.parse(refFileContent))
+    })
+
+    var sample = jsf(schema, refs)
+
+    console.log(sample.username)
+  })
+})
+
+
+
