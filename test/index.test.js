1d70aaa7dc73e0277570befc830013f0a4f0a9db
Amr Eldib <eldib.amr@gmail.com>
2016-04-08 07:17:29 -0700

    Updated test
    Updated test to add better details

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,10 @@
 var agolDataFaker = require('../index');
+var assert = require('assert');
 
 describe("agol-data-faker", function () {
   it("Can generate schema without errors", function () {
-    return agolDataFaker.generateFakeDataForSchema('getItemComment');
+    return agolDataFaker.generateFakeDataForSchema('getItemComment').then(function(f) {
+		return assert.notEqual(JSON.stringify(f), '');
+	});
   });
 });
